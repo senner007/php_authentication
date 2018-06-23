14b240996379e2a879930f63b451e7dc55d2b8ea
senner007 <nielshtg@hotmail.com>
2018-06-23 20:17:23 +0200

    git init

---
+++ login/login.php
@@ -0,0 +1,29 @@
+<?php
+   session_start();
+   header("Access-Control-Allow-Origin: http://localhost:3000"); // should be set to host address on deploy
+   header("Access-Control-Allow-Headers: *"); // TODO : Fix -  https://stackoverflow.com/questions/8719276/cors-with-php-headers
+   header('Access-Control-Allow-Credentials: true');
+   // header('Content-Type: application/json');
+
+   if (isset( $_SESSION['myname'])) {
+     echo "is set!";
+     return;
+   }
+
+   // create password table
+   // hash and salt passwords
+
+    if(isset($_POST['mypass']) && isset($_POST['myname'])) {
+
+
+       if ($_POST['mypass'] == "pass" && $_POST['myname'] == "buller") {
+
+            $_SESSION['myname'] = $_POST['myname'];
+            echo "session set!";
+            return; 
+   
+       }
+    }
+    echo "Login failed!";
+
+?>
